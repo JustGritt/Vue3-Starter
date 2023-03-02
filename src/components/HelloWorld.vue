ae29dc3ab7ffd253516c63487b26338655a7fa5e
Alexis TAN <alexistan9712@gmail.com>
2023-03-02 12:17:01 +0100

    ✨ Add PostCSS & Tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,44 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+defineProps<{ msg: string }>()
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
-  </div>
+	<div class="greetings">
+		<h1 class="green">{{ msg }}</h1>
+		<h3>
+		You’ve successfully created a project with
+		<a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+		<a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+		</h3>
+	</div>
 </template>
-
-<style scoped>
+<style lang="postcss" scoped>
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
+	font-weight: 500;
+	font-size: 2.6rem;
+	top: -10px;
+
+	.greetings {
+		text-align: center;
+
+		@media screen and (min-width: 1024px) {
+			text-align: left;
+
+		}
+	}
 }
 
 h3 {
-  font-size: 1.2rem;
+	font-size: 1.2rem;
+
+	.greetings {
+		text-align: center;
+
+		@media screen and (min-width: 1024px) {
+			text-align: left;
+
+		}
+	}
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
